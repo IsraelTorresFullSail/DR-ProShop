958c2b8e01b7c5be4f818bf3e492410bd6cacb45
Israel Torres <israel.a.torres01@gmail.com>
2021-03-31 00:05:32 -0400

    admin screen functions part1 in progress

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,14 +4,18 @@ import {
   getUserProfile,
   registerUser,
   updateUserProfile,
+  getUsers,
+  deleteUser,
 } from '../controllers/userController.js'
-import { protect } from '../middlewere/authMiddlewere.js'
+import { protect, admin } from '../middlewere/authMiddlewere.js'
 
 const router = express.Router()
 
+router.post('/', registerUser)
+router.get('/', protect, admin, getUsers)
 router.post('/login', authUser)
 router.get('/profile', protect, getUserProfile)
 router.put('/profile', protect, updateUserProfile)
-router.post('/', registerUser)
+router.delete('/:id', protect, admin, deleteUser)
 
 export default router
