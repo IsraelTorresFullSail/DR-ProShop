3fa0a9a48de67f9f3ab96d15fedb1a47e34fa9d2
Israel Torres <israel.a.torres01@gmail.com>
2021-04-01 22:34:08 -0400

    admin delete product completed

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -10,6 +10,21 @@ const getProducts = asyncHandler(async (req, res) => {
 })
 
 // @desc     Fetch single product
+// @route    DELETE /api/products/:id
+// @access   Private/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    await product.remove()
+    res.json({ message: 'Product removed' })
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+})
+
+// @desc     Delete a product
 // @route    GET /api/products/:id
 // @access   Public
 const getProductById = asyncHandler(async (req, res) => {
@@ -23,4 +38,4 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 })
 
-export { getProducts, getProductById }
+export { getProducts, getProductById, deleteProduct }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,11 +2,14 @@ import express from 'express'
 import {
   getProducts,
   getProductById,
+  deleteProduct,
 } from '../controllers/productController.js'
+import { protect, admin } from '../middlewere/authMiddlewere.js'
 
 const router = express.Router()
 
 router.get('/', getProducts)
 router.get('/:id', getProductById)
+router.delete('/:id', protect, admin, deleteProduct)
 
 export default router
