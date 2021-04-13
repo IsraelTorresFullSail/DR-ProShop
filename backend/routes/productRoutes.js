a09a7c8d38a2734fd1942a42ed4c317df7dbb756
Israel Torres <israel.a.torres01@gmail.com>
2021-04-12 23:32:45 -0400

    product reviews, search, pagination and carousel created

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,6 +6,7 @@ import {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts,
 } from '../controllers/productController.js'
 import { protect, admin } from '../middlewere/authMiddlewere.js'
 
@@ -14,6 +15,7 @@ const router = express.Router()
 router.get('/', getProducts)
 router.post('/', protect, admin, createProduct)
 router.post('/:id/reviews', protect, createProductReview)
+router.get('/top', getTopProducts)
 router.get('/:id', getProductById)
 router.delete('/:id', protect, admin, deleteProduct)
 router.put('/:id', protect, admin, updateProduct)
